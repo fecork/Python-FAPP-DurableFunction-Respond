22bbd90f57cdbe4354448b10ee17ec0423e94ba6
Ferney Córdoba <fecork88@gmail.com>
2022-08-17 13:41:08 -0500

    🎉 feat(INIT,-PIPELINE): Se analiza la categoria 19 si hay un pasajero tipo child

--- GptModelPoliciesTpt/__init__.py
+++ GptModelPoliciesTpt/__init__.py
@@ -29,12 +29,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         list_passengers_type.append(passenger_type)
         list_passengers_type = list(set(list_passengers_type))
 
+    is_child = True if "child" in list_passengers_type else False
     parameter_penalty_text = remove_duplicate_passenger(
         parameter_penalty_text, list_passengers_type)
 
+    parameter_penalty_text = extract_passenger(parameter_penalty_text, 'adult')
+
     if parameter_task == "CANCELLATION":
         gpt_response = iterate_penalty_text(
-            parameter_penalty_text, parameter_information)
+            parameter_penalty_text, parameter_information, is_child)
         for elements in gpt_response:
             logging.info(elements["fareBasis"])
 
@@ -69,7 +72,7 @@ def get_parameter(req: object, parameter: str) -> str:
     return parameter_string
 
 
-def iterate_penalty_text(penalty_text: list, parameter_information: str) -> list:
+def iterate_penalty_text(penalty_text: list, parameter_information: str, is_child: bool) -> list:
     """
     iterate over the penalty text and execute the pipeline
     Args: 
@@ -79,12 +82,13 @@ def iterate_penalty_text(penalty_text: list, parameter_information: str) -> list
     """
     lista_respuestas = []
     for dict_penalty in penalty_text:
-        dict_response = iterate_categories(dict_penalty, parameter_information)
+        dict_response = iterate_categories(
+            dict_penalty, parameter_information, is_child)
         lista_respuestas.append(dict_response)
     return lista_respuestas
 
 
-def iterate_categories(dict_penalty: dict, parameter_information: str) -> dict:
+def iterate_categories(dict_penalty: dict, parameter_information: str, is_child: bool) -> dict:
     """
     iterate over the categories and execute the pipeline
     Args: 
@@ -92,18 +96,21 @@ def iterate_categories(dict_penalty: dict, parameter_information: str) -> dict:
     Returns:
         dict penalty
     """
-    categorias = dict_penalty["categories"]
+    result_categories = extract_categories(dict_penalty)
+    text_category_sixteen = result_categories['16']
+    text_category_nineteen = result_categories['19']
+
     dict_respond_categories = {}
-    for dict_category in categorias:
-        code = dict_category['code']
-        if code == '16':
-            text_category_sixteen = dict_category['freeText']
-            model_response = pipeline.execute_concurrent(
-                text_category_sixteen, parameter_information)
-            dict_respond_categories["modelRespond"] = model_response
-            dict_penalty.update(dict_respond_categories)
-            dict_penalty.update({"freeText": text_category_sixteen})
-            del dict_penalty["categories"]
+
+    model_response = pipeline.execute_concurrent(
+        text_category_sixteen, text_category_nineteen, parameter_information, is_child)
+    dict_respond_categories["modelRespond"] = model_response
+    dict_penalty.update(dict_respond_categories)
+    list_free_text = [
+        {"category": 16, "text": text_category_sixteen}, 
+        {"category": 19, "text": text_category_nineteen}]
+    dict_penalty.update({"freeText": list_free_text})
+    del dict_penalty["categories"]
     return dict_penalty
 
 
@@ -136,5 +143,46 @@ def remove_duplicate_passenger(penalty_text: list, list_passenger_type: list) ->
         if passenger_type[0] in list_passenger_type:
             list_passenger_type.pop(
                 list_passenger_type.index(passenger_type[0]))
+            list_clean.append(dict_penalty)
+    return list_clean
+
+
+def extract_categories(dict_penalty: dict) -> dict:
+    """
+    extract categories
+    Args:
+        dict_penalty: json
+    Returns:
+        dict of categories
+    """
+
+    thereis_sixteen = False
+    thereis_nineteen = False
+    categorias = dict_penalty["categories"]
+    for dict_category in categorias:
+        code = dict_category['code']
+        if code == '16' and thereis_sixteen == False:
+            text_category_sixteen = dict_category['freeText']
+            thereis_sixteen = True
+        if code == '19' and thereis_nineteen == False:
+            text_category_nineteen = dict_category['freeText']
+            thereis_nineteen = True
+        if thereis_sixteen and thereis_nineteen:
+            break
+    return {'16': text_category_sixteen, '19': text_category_nineteen}
+
+
+def extract_passenger(penalty_text: dict, type_passenger: str) -> list:
+    """
+    extract adult passenger
+    Args:
+        dict_penalty: json
+    Returns:
+        dict of adult passenger
+    """
+    list_clean = []
+    for dict_penalty in penalty_text:
+        passenger_type = dict_penalty['passengerTypes']
+        if passenger_type[0].lower() == type_passenger:
             list_clean.append(dict_penalty)
     return list_clean
