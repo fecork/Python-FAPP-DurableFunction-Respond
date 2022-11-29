3c82d46d7b1bed9552d92c79d9edeb4e97ef9c01
Ferney Córdoba Canchala <fecork88@gmail.com>
2022-11-29 09:57:11 -0500

    🎉 feat(Adapters): se agrega un nuevo adaptador para amazon >>> ⏰ 3h
    para el proyecto se decide cambiar de Azure a Amazon

---
+++ Adapters/adapter_amazon.py
@@ -0,0 +1,12 @@
+def FunctionName(args: dict) -> dict:
+
+    """
+    Function description.
+    Args: a dictionary with the following keys: value, denomination, currency
+    Returns: a dict with the parameters
+
+    """
+
+    result = args
+
+    return {"result": result}
