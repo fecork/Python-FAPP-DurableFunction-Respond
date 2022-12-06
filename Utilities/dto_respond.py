8f11727017b05e3d837bc26280d9922751daf239
Ferney Córdoba Canchala <fecork88@gmail.com>
2022-12-06 13:01:36 -0500

    🐛 fix(validador-child): se corrige error al validar un menor de edad

--- Utilities/dto_respond.py
+++ Utilities/dto_respond.py
@@ -3,23 +3,23 @@ class Respond:
 
     def __init__(
         self,
+        numberQuestion: int,
         question: str,
         answer: str,
         category: int,
         quote: str,
         freeText: str,
-        numberQuestion: int,
         boolean: bool,
         meanProbability: float,
         value: float,
         denomination: str,
     ) -> None:
+        self.numberQuestion = numberQuestion
         self.question = question
         self.answer = answer
         self.category = category
         self.quote = quote
         self.freeText = freeText
-        self.numberQuestion = numberQuestion
         self.boolean = boolean
         self.meanProbability = meanProbability
         self.value = value
